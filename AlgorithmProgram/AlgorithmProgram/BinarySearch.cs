4b12bfe35d7a0440dc313b4aca1f3d863a3e18f6
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-09-06 19:01:13 -0700

    Added Binary Search of Words

---
+++ AlgorithmProgram/AlgorithmProgram/BinarySearch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AlgorithmProgram
+{
+    class BinarySearch
+    {
+        public static void SearchWord()
+        {
+
+            FileStream fileStream = new FileStream(@"C:\review.txt", FileMode.Open, FileAccess.Read);
+            Console.WriteLine("Searching word in the filepath C:/review.txt");
+            StreamReader reader = new StreamReader(fileStream);
+            string record;
+            string input;
+            Console.WriteLine("Enter word to search");
+            input = Console.ReadLine();
+            try
+            {
+                record = reader.ReadLine();
+                while (record != null)
+                {
+                    if (record.Contains(input))
+                    {
+                        Console.WriteLine("word present in the file");
+                        SearchWord();
+                    }
+                    else
+                    {
+                        Console.WriteLine("word not present");
+                        break;
+                    }
+
+                    record = reader.ReadLine();
+                }
+            }
+            finally
+            {
+                reader.Close();
+                fileStream.Close();
+            }
+        }
+
+        
+    }
+}
--- AlgorithmProgram/AlgorithmProgram/Program.cs
+++ AlgorithmProgram/AlgorithmProgram/Program.cs
@@ -6,17 +6,31 @@ namespace AlgorithmProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter string for permutation: ");
-            string arr = Console.ReadLine();
-            Console.WriteLine("Permutation of string is: ");            
-            Permutation.PerformPermutation(arr, 0, arr.Length - 1);
-            
-            Console.WriteLine("Enter first string:");
-            string arrOne = Console.ReadLine();
-            Console.WriteLine("Enter second string:");
-            string arrTwo = Console.ReadLine();
-            CheckArrayString.CheckString(arrOne, arrTwo);
-            Console.WriteLine("string is" + CheckArrayString.CheckString(arrOne, arrTwo));
+            Console.WriteLine("Enter:1.Permutations of string\t Enter:2.Binary search word from word list");
+            int option = Convert.ToInt32(Console.ReadLine());
+            //bool flag = true;
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter string for permutation: ");
+                    string arr = Console.ReadLine();
+                    Console.WriteLine("Permutation of string is: ");                    
+                    Permutation.PerformPermutation(arr, 0, arr.Length - 1);
+                    
+                    Console.WriteLine("Enter first string:");
+                    string arrOne = Console.ReadLine();
+                    Console.WriteLine("Enter second string:");
+                    string arrTwo = Console.ReadLine();
+                    CheckArrayString.CheckString(arrOne, arrTwo);
+                    Console.WriteLine("string is" + CheckArrayString.CheckString(arrOne, arrTwo));
+                    break;
+                case 2:
+                    BinarySearch.SearchWord();
+
+                    //BinarySearch.WordArray();
+                    Console.WriteLine();
+                    break;
+            }
         }
     }
 }
