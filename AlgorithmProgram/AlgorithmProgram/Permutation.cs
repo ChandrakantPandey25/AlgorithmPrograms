dc3cd44242aeb8bca8cf50541bce3bbdd20384a5
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-09-06 17:38:41 -0700

    Added permutation of Strings

---
+++ AlgorithmProgram/AlgorithmProgram/Permutation.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmProgram
+{
+    class Permutation
+    {
+        public static string SwapChar(string array, int startChar, int endChar)
+        {
+            char temp;
+            char[] charArray = array.ToCharArray();
+            temp = charArray[startChar];
+            charArray[startChar] = charArray[endChar];
+            charArray[endChar] = temp;
+            string newStringForm = new string(charArray);
+            return newStringForm;            
+        }
+
+        public static void PerformPermutation(string str, int start, int end)
+        {
+            if (start == end)
+                Console.WriteLine(str);
+
+            else
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    str = SwapChar(str, start, i);
+                    PerformPermutation(str, start + 1, end);
+                    str = SwapChar(str, start, i);
+                }
+            }
+        }
+
+    }
+    internal class CheckArrayString
+    {
+        public static bool CheckingGenString<T>(T[] strOne, T[] strTwo) where T : IComparable
+        {
+            int res = strOne.Length.CompareTo(strTwo.Length);            
+            if (res == -1)
+                return false;
+            else if (res == 1)
+                return false;
+            else if (res == 0)
+            {
+                for (int i = 0; i < strOne.Length; i++)
+                {
+                    if (strOne[i].Equals(strTwo[i]) == false)
+                        return false;
+
+                }              
+
+            }
+            return true;
+        }
+        public static bool CheckString(string strOne, string strTwo)
+        {
+            char[] arrCharOne = strOne.ToCharArray();
+            char[] arrCharTwo = strTwo.ToCharArray();
+
+            if (arrCharOne.Length != arrCharTwo.Length)
+                return false;            
+
+            if (arrCharOne.Length == arrCharTwo.Length)
+            {
+                Array.Sort(arrCharOne);
+                Array.Sort(arrCharTwo);
+                for (int i = 0; i < arrCharOne.Length; i++)
+                {
+                    if (arrCharOne[i] != arrCharTwo[i])
+                    {                        
+                        return false;
+                    }
+                }                
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
+
---
+++ AlgorithmProgram/AlgorithmProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AlgorithmProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string for permutation: ");
+            string arr = Console.ReadLine();
+            Console.WriteLine("Permutation of string is: ");            
+            Permutation.PerformPermutation(arr, 0, arr.Length - 1);
+            
+            Console.WriteLine("Enter first string:");
+            string arrOne = Console.ReadLine();
+            Console.WriteLine("Enter second string:");
+            string arrTwo = Console.ReadLine();
+            CheckArrayString.CheckString(arrOne, arrTwo);
+            Console.WriteLine("string is" + CheckArrayString.CheckString(arrOne, arrTwo));
+        }
+    }
+}
